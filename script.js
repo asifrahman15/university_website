2bfc28d1bf23a85f2e75bda8e2a2bcee01851b49
Aasif Rahman <asifrahman15@gmail.com>
2022-11-27 14:46:41 +0530

    Clearance Update: 2022 November 27

---
+++ script.js
@@ -0,0 +1,10 @@
+var navLinks = document.getElementById("navLinks");
+
+// Toggle Menu
+function showMenu(){
+    navLinks.style.width = "200px";
+}
+
+function hideMenu(){
+    navLinks.style.width = "0";
+}
